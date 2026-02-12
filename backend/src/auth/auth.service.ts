0136b875d98f6e8263dc124dbebb94ac6bbf5b44
Middle <pourcel.kevin@gmail.com>
2026-02-12 18:42:54 +0100

    Migration du stockage map -> DB avec Prisma. Fait pour auth et User, reste a faire lobby

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -12,32 +12,19 @@ export class AuthService{
 	constructor(
 		private readonly users: UsersService,
 		private readonly jwt: JwtService,
-		private readonly audit:  AuditLoggerService,
+		private readonly audit: AuditLoggerService,
 	){}
 	async register(dto: RegisterDto): Promise<{accessToken: string }>{
 		this.audit.log("auth.register.attempt", {email: dto.email});
-
-		const idCheck = this.users.findByEmail(dto.email);
-		if (idCheck)
-		{
-			this.audit.warn("auth.register.fail", { email: dto.email, reason: "email_taken" });
-			throw new BadRequestException("Email already used");
-		}
-		const usernameCheck = this.users.findByUsername(dto.username);
-		if (usernameCheck)
-		{
-			this.audit.warn("auth.register.fail", { email: dto.email, reason: "username_taken" });
-			throw new BadRequestException("Username already used");
-		}
 		const passwordHash = await bcrypt.hash(dto.password, 10);
-		const user = this.users.create(dto.email, dto.username, passwordHash);
+		const user = await this.users.create(dto.email, dto.username, passwordHash);
 		this.audit.log("auth.register.success", { userId: user.id, email: user.email });
 		const token = this.jwt.sign({ sub : user.id});
 		return {accessToken: token};
 	}
 	async login(dto: LoginDto): Promise<{ accessToken: string }> {
 		this.audit.log("auth.login.attempt", { email: dto.email });
-	const user = this.users.findByEmail(dto.email);
+	const user = await this.users.findByEmail(dto.email);
 	if (!user){
 		this.audit.warn("auth.login.fail", { email: dto.email, reason: "not_found" });
 		throw new UnauthorizedException("Invalid credentials");
@@ -51,5 +38,3 @@ export class AuthService{
 	return { accessToken: this.jwt.sign({ sub: user.id }) };
 	}
 }
-
- 
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,68 +1,63 @@
-import { BadRequestException, Injectable, OnModuleInit } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import * as bcrypt from "bcryptjs";
+import { Injectable, BadRequestException} from "@nestjs/common";
 import { User } from "./types/users";
-import { randomUUID } from "crypto";
+import { PrismaService } from "src/prisma/prisma.service";
+import {Prisma, User as PrismaUser} from "@prisma/client";
 
 @Injectable()
-export class UsersService implements OnModuleInit{
-	private userById:  Map<string, User>;
-	private userIdByEmail: Map<string, string>;
-	private userIdByUsername: Map<string,string>;
-	constructor(private readonly config: ConfigService)
-	{
-		this.userById = new Map <string, User>();
-		this.userIdByEmail = new Map<string,string>();
-		this.userIdByUsername = new Map<string, string>();
+export class UsersService{
+	constructor(
+		private readonly prisma: PrismaService,
+	){}
+		private toDomain(user: PrismaUser): User{
+			return{
+				 	id: user.id,
+					email: user.email,
+					username: user.username,
+					passwordHash: user.passwordHash,
+			};	
 	}
-	async onModuleInit(): Promise<void> {
-		await this.seedTestUserIfNeeded();
+		private isUniqueConstraintError(error: unknown): error is Prisma.PrismaClientKnownRequestError {
+  			return (error instanceof Prisma.PrismaClientKnownRequestError && error.code === "P2002");
 	}
-	private async seedTestUserIfNeeded(): Promise<void> {
-		const email = this.config.get<string>("TEST_USER_EMAIL");
-		const username = this.config.get<string>("TEST_USER_USERNAME");
-		const password = this.config.get<string>("TEST_USER_PASSWORD");
-		console.log("[seed] env present:", !!email, !!username, !!password);
-		console.log("[seed] already exists:", this.userIdByEmail.has(email ?? ""));
-
-	if (!email || !username || !password)
-		return ;
-	if (this.userIdByEmail.has(email))
-		return;
-	const passwordHash = await bcrypt.hash(password, 10);
-	this.create(email, username, passwordHash);
-	console.log("[seed] created user:", email);
+	async create(email:string, username: string, passwordHash: string): Promise<User>{
+		try {
+			const created = await this.prisma.user.create({
+				data: {email, username,  passwordHash},
+		});
+		return (this.toDomain(created));
+		} catch (error: unknown)
+		{
+			if (!this.isUniqueConstraintError(error))
+				throw error;
+			let target = "";
+			if(Array.isArray(error.meta?.target))
+			{
+				target = error.meta.target.map((value) => String(value)).join(",");
+			}
+			if (target.includes("email"))
+				throw new BadRequestException("Email already used");
+    			if (target.includes("username"))
+				throw new BadRequestException("Username already used");
+    			throw new BadRequestException("User already exists"); 
+		}
 	}
-	create(email:string, username: string, passwordHash: string): User{
-		if (this.userIdByEmail.has(email))
-			throw new BadRequestException("Email already used");
-		if (this.userIdByUsername.has(username))
-			throw new BadRequestException("Username already used");
-		const user : User = {
-			id : randomUUID(),
-			email: email,
-			username : username,
-			passwordHash: passwordHash,
-		};
-		this.userById.set(user.id, user);
-		this.userIdByEmail.set(user.email, user.id);
-		this.userIdByUsername.set(user.username, user.id);
-		return (user);
-	}
-	findByUsername(username: string): User | undefined{
-		const userName = this.userIdByUsername.get(username);
-		if (userName === undefined)
+	async findByUsername(username: string): Promise<User | undefined>{
+		const user = await this.prisma.user.findUnique({where: {username}})
+		if (!user)
 			return (undefined);
-		return (this.userById.get(userName));
+		return (this.toDomain(user));
 	}
-	findById(id: string): User | undefined{
-		return (this.userById.get(id));
-	}
-	findByEmail(email: string): User | undefined{
-
-		const userId = this.userIdByEmail.get(email);
-		if (userId === undefined)
+	async findById(id: string): Promise<User | undefined>{
+		const user = await this.prisma.user.findUnique({where: {id}})
+		if (!user)
 			return (undefined);
-		return (this.userById.get(userId));
+		return (this.toDomain(user));
+	}
+	async findByEmail(email: string): Promise<User | undefined>{ 
+
+		const user = await this.prisma.user.findUnique({where: {email}})
+		if (!user)
+			return (undefined);
+		return (this.toDomain(user));
 	}
 }
