608e0eb7304d3cf22ce722dc228988eeb69edb18
Middle <pourcel.kevin@gmail.com>
2026-02-24 16:14:44 +0100

    Ajout de table dans la db

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
 	],
 	server: {
 		host: '0.0.0.0',
-		allowedHosts: ['hometown-gauge-blonde-backup.trycloudflare.com'],
+		allowedHosts: ['power-drainage-thumbs-venice.trycloudflare.com'],
 		port: 5173,
 		watch: {
 			usePolling: true // Requis pour le hot-reload dans Docker
