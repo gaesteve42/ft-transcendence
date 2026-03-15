041f589a783531fe20082a5b77ba85e757e3728e
Gauthier Esteve <gauthier.esteve@gmx.fr>
2026-03-15 17:03:51 +0100

    README_frontend modified

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,7 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
-
+// native file from the installation of Vite, analyzes the code in order to detect errors and reinforce code conventions.
 export default defineConfig([
   globalIgnores(['dist']),
   {
